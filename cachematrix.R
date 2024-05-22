0bb0b940c1e04b016bff9feb865b904bd4e6d342
gmhj <inflow.rasp.0a@icloud.com>
2024-05-22 11:29:23 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(matrix = matrix()) {
+  inverse <- NULL  # Initialize the inverse property
+  
+  ## setMatrix: Set the matrix and reset the cached inverse
+  setMatrix <- function(newMatrix) {
+    matrix <<- newMatrix
+    inverse <<- NULL  # Reset the inverse when the matrix is set
+  }
+  
+  ## getMatrix: Get the value of the matrix
+  getMatrix <- function() matrix
+  
+  ## setInverse: Set the value of the inverse
+  setInverse <- function(invMatrix) inverse <<- invMatrix
+  
+  ## getInverse: Get the value of the inverse
+  getInverse <- function() inverse
+  
+  ## Return a list of the four functions
+  list(setMatrix = setMatrix, getMatrix = getMatrix,
+       setInverse = setInverse, getInverse = getInverse)
 }
 
+## cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix.
+## If the inverse has already been calculated (and the matrix has not changed),
+## then cacheSolve retrieves the inverse from the cache.
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(cacheMatrix, ...) {
+  inverse <- cacheMatrix$getInverse()
+  
+  ## If the inverse is already calculated, return it
+  if (!is.null(inverse)) {
+    message("getting cached data")
+    return(inverse)
+  }
+  
+  ## Otherwise, calculate the inverse
+  data <- cacheMatrix$getMatrix()
+  inverse <- solve(data, ...)  # Compute the inverse
+  cacheMatrix$setInverse(inverse)  # Cache the inverse
+  
+  inverse  # Return the inverse
 }
